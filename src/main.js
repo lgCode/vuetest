c26292c9219b5a44a552bfda5a19c104b984eacd
lgCode <740690924@qq.com>
2021-06-07 21:52:38 +0800

    fix：构建出的组件使用时，prop不能用驼峰式；

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 // import api from "./request/api"
-
 // Vue.prototype.api = api;
 
 Vue.config.productionTip = false;
